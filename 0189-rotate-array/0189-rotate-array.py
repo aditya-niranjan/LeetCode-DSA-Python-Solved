0c6af978fe5c47d870b6102899ced486cc87303c
Aditya - Niranjan <adityaniranjan57@gmail.com>
2025-10-14 21:14:38 +0530

    Time: 0 ms (100%), Space: 22.7 MB (33.35%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k= k % n
+
+        nums[:] = nums[-k:] + nums[:-k]
+
+s1 = Solution()
+arr = [1,2,3,4,5,6,7]
+s1.rotate(arr,3)
