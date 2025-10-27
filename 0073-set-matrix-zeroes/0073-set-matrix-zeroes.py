c292c37159c0018c56a5f0da246921e1556384db
Aditya - Niranjan <adityaniranjan57@gmail.com>
2025-10-27 22:46:35 +0530

    Time: 4 ms (68.11%), Space: 13 MB (74.05%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,34 +1,42 @@
 class Solution(object):
     def setZeroes(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
-        """
-        
         r = len(matrix)
         c = len(matrix[0])
-        
-        for i in range(0,r):
-            for j in range(0,c):
-                if matrix[i][j]==0:
-                    self.Mark_infinity(matrix,i,j)
 
-        for i in range(0,len(matrix)):
-            for j in range(0,len(matrix[0])):
-                  if matrix[i][j] == float("inf"):
-                      matrix[i][j]=0
+        first_row_zero = False
+        first_col_zero = False
+
+        # Check if first row has zero
+        for j in range(c):
+            if matrix[0][j] == 0:
+                first_row_zero = True
+
+        # Check if first column has zero
+        for i in range(r):
+            if matrix[i][0] == 0:
+                first_col_zero = True
+
+        # Mark zeros in first row/col for the rest of matrix
+        for i in range(1, r):
+            for j in range(1, c):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        # Set zeroes except first row & col
+        for i in range(1, r):
+            for j in range(1, c):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        # Handle first row
+        if first_row_zero:
+            for j in range(c):
+                matrix[0][j] = 0
+
+        # Handle first column
+        if first_col_zero:
+            for i in range(r):
+                matrix[i][0] = 0
+
         return matrix
-    
-    
-    def Mark_infinity(self,matrix,rows,cols):
-      
-        r =len(matrix)
-        c = len(matrix[0])
-
-        for i in range(0,r):
-            if matrix[i][cols] != 0:
-                matrix[i][cols]=float("inf")
-
-        for j in range(0,c):
-            if matrix[rows][j] !=0:
-                matrix[rows][j]=float("inf")
