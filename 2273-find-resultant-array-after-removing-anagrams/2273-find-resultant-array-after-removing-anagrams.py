536914fd5329bd1180b78a6553181132683637af
Aditya - Niranjan <adityaniranjan57@gmail.com>
2026-03-05 22:12:05 +0530

    Time: 10 ms (58.47%), Space: 12.3 MB (97.76%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeAnagrams(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        
+        res = [words[0]]
+        
+        for i in range(1, len(words)):
+            if sorted(words[i]) != sorted(res[-1]):
+                res.append(words[i])
+                
+        return res
