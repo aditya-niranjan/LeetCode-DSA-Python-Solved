0e19e5800b33505c896be5e2f86a9f11d1a69158
Aditya - Niranjan <adityaniranjan57@gmail.com>
2026-03-06 20:32:52 +0530

    Time: 2388 ms (10.27%), Space: 13.3 MB (33.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,15 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hash_map = {}
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in hash_map:
-                return [hash_map[diff],i]
-            hash_map[num]=i
-        return []
-
-s1 = Solution()
-print(s1.twoSum([2,7,11,15],9))
-print(s1.twoSum([3,2,4],6))
-print(s1.twoSum([3,3],6))
+        num = len(nums)
+        for i in range(num):
+            for j in range(i+1,num):
+                total = nums[i] + nums[j]
+                if total ==target:
+                    return [i,j]
