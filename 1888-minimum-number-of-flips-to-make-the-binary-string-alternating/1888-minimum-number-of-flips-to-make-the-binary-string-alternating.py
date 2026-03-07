4bb69fe0d74d5cf8b1550edbc9a339726aab1778
Aditya - Niranjan <adityaniranjan57@gmail.com>
2026-03-07 20:46:27 +0530

    Time: 751 ms (51.85%), Space: 20.9 MB (55.56%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -1,16 +1,41 @@
-class Solution:
-    def minFlips(self, s: str) -> int:
+class Solution(object):
+    def minFlips(self, s):
         n = len(s)
-        res = n
-        op = [0, 0]
-
-        for i in range(n):
-            op[(ord(s[i]) ^ i) & 1] += 1
-
-        for i in range(n):
-            c = ord(s[i])
-            op[(c ^ i) & 1] -= 1
-            op[(c ^ (n + i)) & 1] += 1
-            res = min(res, min(op))
-
+        s = s + s
+        
+        alt1 = ""
+        alt2 = ""
+        
+        for i in range(len(s)):
+            if i % 2 == 0:
+                alt1 += "0"
+                alt2 += "1"
+            else:
+                alt1 += "1"
+                alt2 += "0"
+        
+        res = float('inf')
+        diff1 = 0
+        diff2 = 0
+        left = 0
+        
+        for right in range(len(s)):
+            
+            if s[right] != alt1[right]:
+                diff1 += 1
+            if s[right] != alt2[right]:
+                diff2 += 1
+            
+            if right - left + 1 > n:
+                
+                if s[left] != alt1[left]:
+                    diff1 -= 1
+                if s[left] != alt2[left]:
+                    diff2 -= 1
+                
+                left += 1
+            
+            if right - left + 1 == n:
+                res = min(res, diff1, diff2)
+        
         return res
